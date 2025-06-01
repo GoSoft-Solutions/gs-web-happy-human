7b072c8eaf1139b75ecf121fa0952975afab768f
Israel Jesús <jesus2102.garcia@gmail.com>
2025-06-01 00:21:02 -0600

    prueba de cambios

---
+++ frontend/app/components/First_section/prueba.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Endsection: React.FC = () => {
+  return (
+    <div className="flex flex-col w-full mx-auto items-center relative bg-[#272F54] min-h-screen"></div>
+  )
+}
